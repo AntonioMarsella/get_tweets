d5bdc57e7aaece0ce79c1fd0520c6dc803f5dee5
Antonio <antonio.m@santiment.net>
2022-10-28 23:49:24 +0200

    Initial commit

---
+++ app_.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, request, jsonify
+import tweepy
+import os
+from dotenv import load_dotenv
+from app import *
+import sys
+
+sys.path.append("./")
+
+load_dotenv()
+
+
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_KEY_SECRET")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    print(request.args)
+    handle = request.args.get("handle", None)
+    print("You requested", handle)
+    return jsonify({"results": list(get_all_tweets_json(handle))})
+
+
+app.run()
